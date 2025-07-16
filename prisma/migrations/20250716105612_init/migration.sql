e7ca5df1de61cf6ceaef98d1f298a07e4191f37b
serin4103 <serin4103@gmail.com>
2025-07-16 19:57:09 +0900

    Add initial migration for User table

---
+++ prisma/migrations/20250716105612_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `win` INTEGER NOT NULL DEFAULT 0,
+    `lose` INTEGER NOT NULL DEFAULT 0,
+    `profilePicture` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
